55ab8e2324cecd6f446819f0f1456191b8b9108c
ARIJHO <128059950+ARIJHO@users.noreply.github.com>
2024-05-06 08:34:25 -0300

    Add files via upload

--- A1/main.js
+++ A1/main.js
@@ -2,6 +2,8 @@ const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2024-10-05T00:00:00");
+const tempoObjetivo3 = new Date("2024-10-05T00:00:00");
 const tempoAtual= new Date;
 //Declara quais elementos vão ser lidos. O script vai ler todos que tiverem a classe "botao".
 
@@ -19,4 +21,51 @@ for(let i=0;i <botoes.length;i++){
     }
 }
 
-contadores[0].textContent=tempoObjetivo1-tempoAtual;
+contadores[0].textContent=calculaTempo(tempoObjetivo1);
+
+function calculaTempo(tempoObjetivo){
+    let tempoAtual=new Date();
+    let tempoFinal= tempoObjetivo-tempoAtual;
+    let segundos=Math.floor(tempoFinal/1000);
+    let minutos=Math.floor(segundos/60);
+    let horas=Math.floor(minutos/60);
+    let dias=Math.floor(horas/24);
+
+    segundos%=60;
+    minutos%=60;
+    horas%=24;
+
+    return dias+"dias"+horas+"horas"+minutos+"minutos"+segundos+"segundos";
+}
+contadores[1].textContent=calculaTempo(tempoObjetivo2);
+
+function calculaTempo(tempoObjetivo){
+    let tempoAtual=new Date();
+    let tempoFinal= tempoObjetivo-tempoAtual;
+    let segundos=Math.floor(tempoFinal/1000);
+    let minutos=Math.floor(segundos/60);
+    let horas=Math.floor(minutos/60);
+    let dias=Math.floor(horas/24);
+
+    segundos%=60;
+    minutos%=60;
+    horas%=24;
+
+    return dias+"dias"+horas+"horas"+minutos+"minutos"+segundos+"segundos";
+}
+contadores[2].textContent=calculaTempo(tempoObjetivo3);
+
+function calculaTempo(tempoObjetivo){
+    let tempoAtual=new Date();
+    let tempoFinal= tempoObjetivo-tempoAtual;
+    let segundos=Math.floor(tempoFinal/1000);
+    let minutos=Math.floor(segundos/60);
+    let horas=Math.floor(minutos/60);
+    let dias=Math.floor(horas/24);
+
+    segundos%=60;
+    minutos%=60;
+    horas%=24;
+
+    return dias+"dias"+horas+"horas"+minutos+"minutos"+segundos+"segundos";
+}
